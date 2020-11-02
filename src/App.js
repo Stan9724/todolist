4d108cce1d219e836cb499d4682b9a2668724efb
Stan9724 <boiz.tristan@gmail.com>
2020-11-02 11:14:30 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Todo from './Todo'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Todo />
+      </div>
+    );
+  }
+}
+
+export default App;
